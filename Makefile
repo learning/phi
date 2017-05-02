fe683ac5655c06da84f1bcf4c8b6c4cc68431c66
Learning <liangyongning@gmail.com>
2017-05-02 17:57:33 +0800

    add xcode project

--- Makefile
+++ Makefile
@@ -7,3 +7,8 @@ clean:
 
 linux: clean
 	cc $(SRC)/main.linux.c -o $(DIST)/phi `pkg-config --cflags --libs gtk+-2.0`
+	$(DIST)/phi
+
+macos: clean
+	xcodebuild
+	open $(DIST)/Release/phi.app
---
+++ src/main.macos.m
@@ -0,0 +1,51 @@
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+@end
+
+@implementation AppDelegate
+
+- (void)windowCloseHandler {
+  NSLog(@"windowCloseHandler");
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+  [[NSNotificationCenter defaultCenter]
+    addObserver:[NSObject new]
+    selector:@selector(windowCloseHandler)
+    name:NSWindowWillCloseNotification
+    object:nil];
+  NSLog(@"applicationDidFinishLaunching.");
+}
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+  NSLog(@"applicationWillTerminate.");
+}
+
+@end
+
+
+int main (int argc, const char * argv[])
+{
+  // Create a shared app instance.
+  // This will initialize the global variable
+  // 'NSApp' with the application instance.
+  [NSApplication sharedApplication];
+
+  // Set app to front
+  [NSApp setActivationPolicy:NSApplicationActivationPolicyRegular];
+  [NSApp activateIgnoringOtherApps:YES];
+
+  // Remove default menu items
+  [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"NSDisabledDictationMenuItem"];
+  [[NSUserDefaults standardUserDefaults] setBool:YES forKey:@"NSDisabledCharacterPaletteMenuItem"];
+
+  // Entry point
+
+  AppDelegate *appDelegate = [[AppDelegate alloc] init];
+  [NSApp setDelegate:appDelegate];
+
+  [NSApp run];
+
+  return 0;
+}
