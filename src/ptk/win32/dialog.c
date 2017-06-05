b81a9f46958cee9f6dcab284ca55c48632bbbf1a
Learning <liangyongning@gmail.com>
2017-06-05 15:32:44 +0800

    add show dialog in windows

--- src/ptk/win32/dialog.c
+++ src/ptk/win32/dialog.c
@@ -1,4 +1,10 @@
 #include "../dialog.h"
 
 void ptk_show_dialog(PtkWindow *window, char message[]) {
+  MessageBox(
+    window->instance,
+    message,
+    "Phi",
+    MB_ICONQUESTION | MB_OK | MB_APPLMODAL
+  );
 }
